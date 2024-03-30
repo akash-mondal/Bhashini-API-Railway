699b2487331364e2cb8c56ab8f14392c3977f8c9
Akash Mondal <96812236+akash-mondal@users.noreply.github.com>
2024-03-30 11:52:08 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -16,9 +16,8 @@ import helloRoute from "./routes/helloRouter.js";
 
 const app = express();
 
-// parse json request body
-app.use(express.json());
-
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 // enable cors
 app.use(cors());
 
